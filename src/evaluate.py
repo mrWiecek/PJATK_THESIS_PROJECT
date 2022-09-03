c73fd9e8c390c2ce3634247859c1f0b9fd0919b5
Mateusz Więckowski <mr.mateuszwieckowski@gmail.com>
2022-09-03 17:53:07 +0200

    evaluate

--- src/evaluate.py
+++ src/evaluate.py
@@ -7,7 +7,7 @@ import json
 
 params = yaml.safe_load(open("params.yaml"))["evaluate"]
 features_test_ds = tf.data.experimental.load(os.path.join('data/features', "test"))
-model = keras.models.load_model('models')
+model = keras.models.load_model('models/mlops-thesis-text-classification')
 
 
 metrics_dict = model.evaluate(features_test_ds, return_dict=True)
