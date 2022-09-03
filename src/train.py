13cbca6c0cb4e673c353a2c98041007870e11d6d
Mateusz Więckowski <mr.mateuszwieckowski@gmail.com>
2022-09-03 15:08:06 +0200

    train changes

--- src/train.py
+++ src/train.py
@@ -5,10 +5,10 @@ import yaml
 import os
 import fire
 import onnx
-import keras2onnx
+import tf2onnx
 
 params = yaml.safe_load(open(os.path.join(GLOBALS["BASE_DIR"], "params.yaml")))
-params_features = params["features"]
+params_features = params["featurize"]
 params_train = params["train"]
 
 os.environ['TF_KERAS'] = '1'
@@ -20,7 +20,7 @@ def get_data(data_filepath):
     features_val_ds = tf.data.experimental.load(os.path.join(data_filepath, "val"))
     return features_train_ds, features_test_ds, features_val_ds
 
-def train(model_filepath, feature_data_filepath='data/features'):
+def train(model_filepath='models', feature_data_filepath='data/features'):
     train_ds, test_ds, val_ds = get_data(feature_data_filepath)
     # Do async prefetching / buffering of the data for best performance on GPU.
     train_ds = train_ds.cache().prefetch(buffer_size=10)
@@ -50,15 +50,18 @@ def train(model_filepath, feature_data_filepath='data/features'):
     predictions = layers.Dense(1, activation="sigmoid", name="predictions")(x)
 
     model = tf.keras.Model(inputs, predictions, name="mlops-thesis-text-classification")
-
     # Compile the model with binary crossentropy loss and an adam optimizer.
     model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
     # Fit the model using the train and test datasets.
     model.fit(train_ds, validation_data=val_ds, epochs=params_train['epochs'])
 
-    onnx_model = keras2onnx.convert_keras(model, f'{model.name}')
-    onnx.save_model(onnx_model, f'{model.name}.onnx', location=model_filepath)
+    # Save model
+    input_signature = [tf.TensorSpec([1, 7, 7, 128], tf.float32, name=f'{model.name}-sig')]
+
+    # Use from_function for tf functions
+    onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=13)
+    onnx.save(onnx_model, f"{model.name}.onnx")
 
 if __name__ == "__main__":
     fire.Fire(train)
