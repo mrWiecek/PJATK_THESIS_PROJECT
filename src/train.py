1f4a6146d5ffd80c7c9e415d963ab0fff30d04bb
Mateusz Więckowski <mr.mateuszwieckowski@gmail.com>
2022-09-03 16:51:16 +0200

    added metrics

--- src/train.py
+++ src/train.py
@@ -6,12 +6,17 @@ import os
 import fire
 import onnx
 import tf2onnx
+from dvclive.keras import DvcLiveCallback
+from dvclive import Live
+
+live = Live("training_metrics")
+
 
 params = yaml.safe_load(open(os.path.join(GLOBALS["BASE_DIR"], "params.yaml")))
 params_features = params["featurize"]
 params_train = params["train"]
 
-os.environ['TF_KERAS'] = '1'
+os.environ["TF_KERAS"] = "1"
 
 
 def get_data(data_filepath):
@@ -20,7 +25,8 @@ def get_data(data_filepath):
     features_val_ds = tf.data.experimental.load(os.path.join(data_filepath, "val"))
     return features_train_ds, features_test_ds, features_val_ds
 
-def train(feature_data_filepath='data/features', model_filepath='models'):
+
+def train(feature_data_filepath="data/features", model_filepath="models"):
     train_ds, test_ds, val_ds = get_data(feature_data_filepath)
     # Do async prefetching / buffering of the data for best performance on GPU.
     train_ds = train_ds.cache().prefetch(buffer_size=10)
@@ -35,16 +41,28 @@ def train(feature_data_filepath='data/features', model_filepath='models'):
     x = layers.Embedding(
         params_features["max_features"], params_features["embedding_dim"]
     )(inputs)
-    x = layers.Dropout(params_train['model_dropout'])(x)
+    x = layers.Dropout(params_train["model_dropout"])(x)
 
     # Conv1D + global max pooling
-    x = layers.Conv1D(params_train['model_kernel_size'], params_train['model_padding'], padding="valid", activation="relu", strides=params_train['model_strides'])(x)
-    x = layers.Conv1D(params_train['model_kernel_size'], params_train['model_padding'], padding="valid", activation="relu", strides=params_train['model_strides'])(x)
+    x = layers.Conv1D(
+        params_train["model_kernel_size"],
+        params_train["model_padding"],
+        padding="valid",
+        activation="relu",
+        strides=params_train["model_strides"],
+    )(x)
+    x = layers.Conv1D(
+        params_train["model_kernel_size"],
+        params_train["model_padding"],
+        padding="valid",
+        activation="relu",
+        strides=params_train["model_strides"],
+    )(x)
     x = layers.GlobalMaxPooling1D()(x)
 
     # We add a vanilla hidden layer:
-    x = layers.Dense(params_train['model_kernel_size'], activation="relu")(x)
-    x = layers.Dropout(params_train['model_dropout'])(x)
+    x = layers.Dense(params_train["model_kernel_size"], activation="relu")(x)
+    x = layers.Dropout(params_train["model_dropout"])(x)
 
     # We project onto a single unit output layer, and squash it with a sigmoid:
     predictions = layers.Dense(1, activation="sigmoid", name="predictions")(x)
@@ -54,14 +72,22 @@ def train(feature_data_filepath='data/features', model_filepath='models'):
     model.compile(loss="binary_crossentropy", optimizer="adam", metrics=["accuracy"])
 
     # Fit the model using the train and test datasets.
-    model.fit(train_ds, validation_data=val_ds, epochs=params_train['epochs'])
+    model.fit(
+        train_ds,
+        validation_data=val_ds,
+        epochs=params_train["epochs"],
+        callbacks=[DvcLiveCallback()],
+    )
 
     # Save model
-    input_signature = [tf.TensorSpec([None, None], tf.float32, name=f'{model.name}-sig')]
+    input_signature = [
+        tf.TensorSpec([None, None], tf.float32, name=f"{model.name}-sig")
+    ]
 
     # Use from_function for tf functions
     onnx_model, _ = tf2onnx.convert.from_keras(model, input_signature, opset=13)
     onnx.save(onnx_model, f"models/{model.name}.onnx")
+
 
 if __name__ == "__main__":
     fire.Fire(train)
